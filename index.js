6c322500c9c50e7f7219e7f1794f3e4522f82171
Abhishek <malveabhishek2000@gmail.com>
2024-03-27 23:08:09 +0530

    first commet

---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+
+const express = require('express')
+
+
+const app = express()
+
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send(`abishekmalve27`)
+})
+
+app.get(`/login`,(req, res) =>{
+    res.send(`<h1>"log in at abhishek malve"</h1>`)
+})
+
+ app.get(`/youtube`, (req, res) =>{
+    res.send(`<h2>chai aur code</h2>`)
+ })
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
